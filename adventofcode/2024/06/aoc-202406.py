9b04bc8016786026fd500035fbfdec18c75a9ab3
LouriNoel <60841793+LouriNoel@users.noreply.github.com>
2024-12-22 19:03:14 +0100

    [AOC 2024/06] Update

--- adventofcode/2024/06/aoc-202406.py
+++ adventofcode/2024/06/aoc-202406.py
@@ -8,47 +8,42 @@ filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input")
 
 lines = read(filepath)
 
-DIRS = {"^": "UP",
-        ">": "RIGHT",
-        "v": "DOWN",
-        "<": "LEFT"}
-
 
 def move(grid, x, y, d):
-    # Move or turn once
+    """ Make the guard move forward once, or turn in-place once if an obstacle is in the way. """
 
     W, H = len(grid[0]), len(grid)
 
-    if d == "UP":
+    if d == "^":
         if y != 0 and grid[y-1][x] == "#":
-            d = "RIGHT"
+            d = ">"
             grid[y][x] = ">"
         else:
             y = y-1
             if y > 0:
                 grid[y][x] = "^"
 
-    elif d == "RIGHT":
+    elif d == ">":
         if x != W-1 and grid[y][x+1] == "#":
-            d = "DOWN"
+            d = "v"
             grid[y][x] = "v"
         else:
             x = x+1
             if x < W:
                 grid[y][x] = ">"
 
-    elif d == "DOWN":
+    elif d == "v":
         if y != H-1 and grid[y+1][x] == "#":
-            d = "LEFT"
+            d = "<"
             grid[y][x] = "<"
         else:
             y = y+1
             if y < H:
                 grid[y][x] = "v"
 
-    elif d == "LEFT":
+    elif d == "<":
         if x != 0 and grid[y][x-1] == "#":
-            d = "UP"
+            d = "^"
             grid[y][x] = "^"
         else:
             x = x-1
@@ -58,46 +53,36 @@ def move(grid, x, y, d):
     return x, y, d
 
 
-def star1():
-    grid = [[c for c in line] for line in lines]
-    W, H = len(grid[0]), len(grid)
+grid = [[c for c in line] for line in lines]
+W, H = len(grid[0]), len(grid)
 
-    # find the guard's position and direction
-    x, y = [(px, py) for py, row in enumerate(grid) for px, c in enumerate(row) if c not in ".#"][0]
-    d = DIRS[grid[y][x]]
+# Find the guard's position and direction
+x, y = [(px, py) for py, row in enumerate(grid) for px, c in enumerate(row) if c not in ".#"][0]
+d = grid[y][x]  # ^ > v <
 
-    # Make the guard move until they leave the area
-    while 0 <= x < W and 0 <= y < H:
-        x, y, d = move(grid, x, y, d)
-
-    count = sum(1 for row in grid for c in row if c not in ".#")
-    print(f" *:{count}")
+# Initial values
+x0, y0, d0 = x, y, d
 
 
-# Takes too long, but still gives the right result
-def star2():
-    grid = [[c for c in line] for line in lines]
-    W, H = len(grid[0]), len(grid)
+# Make the guard move a first time until they leave the area
+candidates = set()
+while 0 <= x < W and 0 <= y < H:
+    candidates.add((x, y))
+    x, y, d = move(grid, x, y, d)  # also write the path on the grid
 
-    # find the guard's position and direction
-    x, y = [(px, py) for py, row in enumerate(grid) for px, c in enumerate(row) if c not in ".#"][0]
-    d = DIRS[grid[y][x]]
 
-    x0, y0, d0, c0 = x, y, d, grid[y][x]
+if star != 2:
+    print(f" *:{len(candidates)}")
 
-    candidates = []
-    G = [[c for c in row] for row in grid]  # modifiable copy
 
-    # Make the guard move a first time
-    while 0 <= x < W and 0 <= y < H:
-        x, y, d = move(G, x, y, d)
-        candidates = [(_x, _y) for _y, _row in enumerate(G) for _x, _c in enumerate(_row) if _c not in ".#"]
-        candidates.remove((x0, y0))
+if star != 1:
+    # 1. The initial position (x0,y0) is not required to be part of a loop
+    # 2. An obstacle is only useful when placed on the original path (to make it deviate)
 
-    # An obstacle is only useful when placed on the original path (to make it deviate)
+    candidates.remove((x0, y0))  # we cannot place an obstacle on the guard's initial position
 
-    count = 0
-    for ox, oy in candidates:
+    nb_loops = 0
+    for ox, oy in candidates:  # Try to place an obstacle at each position on the original path of the guard
         print(f"- ({ox}, {oy})")
 
         grid[oy][ox] = "#"
@@ -107,21 +92,12 @@ def star2():
         while 0 <= x < W and 0 <= y < H:
             x, y, d = move(grid, x, y, d)
             if (x, y, d) in visited:
-                count += 1
-                print(f"loop found! count={count}, ox={ox}, oy={oy}")
+                nb_loops += 1
+                print(f"loop found! count={nb_loops}, ox={ox}, oy={oy}")
                 break
             visited.append((x, y, d))
 
-        # clear
+        # Remove the obstacle for the next try
         grid[oy][ox] = "."
-        for _y, _row in enumerate(grid):
-            for _x, _c in enumerate(_row):
-                if _c not in ".#":
-                    grid[_y][_x] = "."
-        grid[y0][x0] = c0
 
-    print(f"**:{count}")
-
-
-star1()
-star2()
+    print(f"**:{nb_loops}")
