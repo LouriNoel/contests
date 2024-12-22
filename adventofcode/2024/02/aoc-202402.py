509d1dc162c75e18dbc7e8147d69712fadd9139f
LouriNoel <60841793+LouriNoel@users.noreply.github.com>
2024-12-22 14:58:04 +0100

    [AOC 2024/02] Update

--- adventofcode/2024/02/aoc-202402.py
+++ adventofcode/2024/02/aoc-202402.py
@@ -1,33 +1,35 @@
 import os
 import sys
 
-from utils.sylis import read
+from utils.sylis import read, numbers
 
 star = int(sys.argv[1]) if len(sys.argv) == 2 else 0
 filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "input")
 
 lines = read(filepath)
 
-reports = [[int(x) for x in line.split()] for line in lines]
+def is_safe(levels: list[int]) -> bool:
+    #differences = [b - a for a, b in zip(levels[:-1], levels[1:])]
+    differences = [levels[i] - levels[i-1] for i in range(1, len(levels))]
+    return ((all(delta >= 0 for delta in differences) or all(delta <= 0 for delta in differences))
+            and all(delta != 0 and abs(delta) <= 3 for delta in differences))
 
-def check(report):
-    diffs = [report[i+1] - report[i] for i in range(len(report)-1)]
-    a = [abs(x) for x in diffs]
-    # check if all differences are positive or negative, and their absolute value is within the acceptable range
-    return (all(x >= 0 for x in diffs) or all(x <= 0 for x in diffs)) and min(a) >= 1 and max(a) <= 3
-
-safe_reports = [r for r in reports if check(r)]
-print(f" *: {len(safe_reports)}")
-
-def check_damp(report):
-    if check(report):  # already safe
-        return True
+def is_damp(report: list[int]) -> bool:
     for i in range(len(report)):
         damp = report.copy()
         damp.pop(i)
-        if check(damp):  # check if the report is safe when removing the i-th element, for any i
+        if is_safe(damp):  # check if the report is safe when removing the i-th element, for any i
             return True
     return False
 
-damp_safe_reports = [r for r in reports if check_damp(r)]
-print(f"**: {len(damp_safe_reports)}")
+
+reports = [numbers(line) for line in lines]
+safeness = [is_safe(report) for report in reports]
+
+if star != 2:
+    safe_count = safeness.count(True)
+    print(f" *: {safe_count}")
+
+if star != 1:
+    safe_or_damp_count = sum(1 for i, report in enumerate(reports) if safeness[i] or is_damp(report))
+    print(f"**: {safe_or_damp_count}")
